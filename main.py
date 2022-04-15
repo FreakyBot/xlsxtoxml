acbedcd67ce04d8bac6b1ba81c25e623b9b42065
kkaletka <kamil.kaletka@salesmanago.com>
2022-04-15 12:35:12 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from functools import wraps
+
+import et as et
+import lxml
+import pandas as pd
+from lxml import etree as et
+from lxml.etree import CDATA
+
+raw_data = pd.read_excel(r'/home/benhauer-dev/PycharmProjects/pythonProject2/output.xlsx')
+root = et.Element('document')
+
+for row in raw_data.iterrows():
+    root_tags = et.SubElement(root, 'root')  # === > Root name
+    # These are the tag names for each row (SECTION 1)
+    Column_heading_1 = et.SubElement(root_tags, 'sku')
+    Column_heading_2 = et.SubElement(root_tags, 'product_url')
+    Column_heading_3 = et.SubElement(root_tags, 'name_ar')
+    Column_heading_4 = et.SubElement(root_tags, 'image_url')
+    Column_heading_5 = et.SubElement(root_tags, 'price')
+    Column_heading_6 = et.SubElement(root_tags, 'color')
+    Column_heading_7 = et.SubElement(root_tags, 'size')
+    Column_heading_8 = et.SubElement(root_tags, 'age')
+    Column_heading_9 = et.SubElement(root_tags, 'design')
+    Column_heading_10 = et.SubElement(root_tags, 'numberofpaces')
+    Column_heading_11 = et.SubElement(root_tags, 'Package')
+    Column_heading_12 = et.SubElement(root_tags, 'description_ar')
+    Column_heading_13 = et.SubElement(root_tags, 'categories')
+
+    # The values inside the [] are the raw file column headings.(SECTION 2)
+    Column_heading_1.text = CDATA(str(row[1]['sku']))
+    Column_heading_2.text = CDATA(str(row[1]['product_url']))
+    Column_heading_3.text = CDATA(str(row[1]['name_ar']))
+    Column_heading_4.text = CDATA(str(row[1]['image_url']))
+    Column_heading_5.text = CDATA(str(row[1]['price']))
+    Column_heading_6.text = CDATA(str(row[1]['color']))
+    Column_heading_7.text = CDATA(str(row[1]['size']))
+    Column_heading_8.text = CDATA(str(row[1]['age']))
+    Column_heading_9.text = CDATA(str(row[1]['design']))
+    Column_heading_10.text = CDATA(str(row[1]['number of paces']))
+    Column_heading_11.text = CDATA(str(row[1]['Package']))
+    Column_heading_12.text = CDATA(str(row[1]['description_ar']))
+    Column_heading_13.text = CDATA(str(row[1]['categories']))
+
+
+tree = et.ElementTree(root)
+et.indent(tree, space="\t", level=0)
+tree.write('output.xml', encoding="utf-8")
